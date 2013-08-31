ec7d16d217532908bfdb831f910675763e68d469
Victor Stabile <vmstabile@gmail.com>
2013-08-31 16:27:27 -0300

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estacionamento::Application.config.secret_token = '05cd1e9ed02ab9dfd188060cb9aea0430e0651e7df8142a3dad83473e2b514807ccf6ae1ec569e3d3a7bc5ea62dc666aac3e538c6e3005c463504f3c1be11e39'
